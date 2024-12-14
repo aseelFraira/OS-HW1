06bd297e9d7b07893f176b68d5a1ca19f6bf2b98
nour1604 <nour.saad@campus.technion.ac.il>
2024-12-14 17:16:21 +0200

    fixing fg cmd

--- skeleton_smash/Commands.cpp
+++ skeleton_smash/Commands.cpp
@@ -444,7 +444,7 @@ void WhoAmICommand::execute() {
         size_t field_start = 0, field_end;
         std::string fields[7];
         int field_index = 0;
-
+        std::cout << "Fields: " << std::endl;
         while (field_index < 7 && (field_end = line.find(':', field_start)) != std::string::npos) {
             fields[field_index] = line.substr(field_start, field_end - field_start);
             field_start = field_end + 1;
