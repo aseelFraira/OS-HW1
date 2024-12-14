d949d1b92ae8a93bec8188e5889f9f454de6ce78
nour1604 <nour.saad@campus.technion.ac.il>
2024-12-14 18:27:50 +0200

    Signals

--- skeleton_smash/signals.cpp
+++ skeleton_smash/signals.cpp
@@ -5,6 +5,19 @@
 
 using namespace std;
 
-void ctrlCHandler(int sig_num) {
-    // TODO: Add your implementation
+void ctrlCHandler(int sig_num)
+{
+    std::cout << "smash: got ctrl-C\n";
+    SmallShell &smash = SmallShell::getInstance();
+    if (smash.getCurrForegroundPID() != -1)
+    {
+        if (kill(smash.getCurrForegroundPID(), SIGKILL) == -1)
+        {
+            perror("smash error: kill failed");
+            return;
+        }
+        cout << "smash: process " << smash.getCurrForegroundPID() << " was killed\n";
+        smash.setCurrForegroundPID(-1);
+    }
 }
+
